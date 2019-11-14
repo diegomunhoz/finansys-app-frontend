3e53a3446926cbbcf304d50accea4d778e6c2420
Diego Munhoz <diego.fatec@hotmail.com>
2019-11-14 13:11:47 -0300

    Adicionando barra de navegação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
