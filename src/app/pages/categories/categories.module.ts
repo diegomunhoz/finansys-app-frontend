29d45d82c08c3cc691269d4e7f59307dd6191e9e
Diego Munhoz <diego.fatec@hotmail.com>
2019-11-15 13:23:43 -0300

    Implementando a base da classe CategoryForm

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
@@ -7,10 +8,11 @@ import { CategoryFormComponent } from './category-form/category-form.component';
 
 
 @NgModule({
-  declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
-  ]
+    CategoriesRoutingModule,
+    ReactiveFormsModule
+  ],
+  declarations: [CategoryListComponent, CategoryFormComponent]
 })
 export class CategoriesModule { }
