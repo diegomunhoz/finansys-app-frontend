d617b3cfca5d1eb445e736c05d41c17766f3062e
Diego Munhoz <diego.fatec@hotmail.com>
2019-11-17 13:59:40 -0300

    Utilizando o componente bread-crumb nas paginas do projeto

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
+    //Modulos compartilhados
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
+
+    //Componentes Compartilhados
     BreadCrumbComponent
   ],
   declarations: [BreadCrumbComponent]
