e8b696cc0a9b4265d6e94b4a4e5faa34fbcf476c
Diego Munhoz <diego.fatec@hotmail.com>
2019-11-17 15:36:50 -0300

    Criando a base do modulo e componente de Relatórios

---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports/reports.component';
+
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [
+    SharedModule,
+    ReportsRoutingModule
+  ]
+})
+export class ReportsModule { }
