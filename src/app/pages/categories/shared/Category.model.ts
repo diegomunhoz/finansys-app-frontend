d7aad878bc736a5074f1c878cc2237ce11f89917
Diego Munhoz <diego.fatec@hotmail.com>
2019-11-17 09:44:14 -0300

    Implementando injetor e refatorando CategoryService

--- src/app/pages/categories/shared/Category.model.ts
+++ src/app/pages/categories/shared/Category.model.ts
@@ -1,11 +1,11 @@
 import { BaseResourceModel } from '../../../shared/models/base-resource.model';
 
-export class Category exteds BaseResourceModel {
+export class Category extends BaseResourceModel {
   constructor(
     public id?: number,
     public name?: string,
     public description?: string
   ){
-    super();
+    super()
   }
 }
